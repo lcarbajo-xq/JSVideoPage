a1f22a85ad190b0848712a9b9b03c19b8210da18
lcarbajo_xq <lcarbajo_xq@gmail.com>
2018-08-10 15:01:32 +0200

    Al dia hata forumlarios y modales...

--- src/js/home.js
+++ src/js/home.js
@@ -68,9 +68,51 @@ const getUser = new Promise(function(todoBien,todoMal) {
           return data
         }
 
-      const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
-      const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
-      const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
+      const $form = document.getElementById('form');
+      const $home = document.getElementById('home');
+      const $featuringContainer = document.getElementById('featuring');
+
+      function setAttributes ($element, attributes) {
+        for (const attribute in attributes) {
+          $element.setAttribute(attribute, attributes[attribute])
+        }
+      }
+      const BASE_API = 'https://yts.am/api/v2/';
+
+      function featuringTemplate(peli) {
+        return (
+          `<div class="featuring">
+            <div class="featuring-image">
+              <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+            </div>
+            <div class="featuring-content">
+              <p class="featuring-title">Pelicula encontrada</p>
+              <p class="featuring-album">${peli.title}</p>
+            </div>
+          </div>`
+        )
+      }
+
+      $form.addEventListener('submit', async function(event) {
+        event.preventDefault();
+        $home.classList.add('search-active');
+        const $loader = document.createElement('img');
+        setAttributes($loader, {
+          src: 'src/images/loader.gif',
+          height: 50,
+          width: 50,
+        })
+
+        $featuringContainer.append($loader);
+        const data = new FormData($form);
+        const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+        const HTMLString = featuringTemplate(peli.data.movies[0]);
+        $featuringContainer.innerHTML = HTMLString;
+      })
+
+      const actionList = await getData(`${BASE_API}list_movies.json?genre=action`)
+      const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`)
+      const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`)
 
       function videoItemTemplate (movie) {
         return (`<div class="primaryPlaylistItem">
@@ -84,24 +126,52 @@ const getUser = new Promise(function(todoBien,todoMal) {
         )
       }
 
-      actionList.data.movies.forEach((movie) => {
-        debugger
-        const HTMLString = videoItemTemplate(movie);
-        console.log(HTMLString);
-      })
+      function createTemplate(HTMLString) {
+        const html = document.implementation.createHTMLDocument();
+        html.body.innerHTML = HTMLString;
+        return html.body.children[0];
+      }
+
+      const $modal    = document.getElementById('modal');
+      const $overlay  = document.getElementById('overlay');
+
+      function showModal() {
+        $overlay.classList.add('active');
+        $modal.style.animation = 'modalIn .8s forwards';
+      }
+
+      const $hideModal = document.getElementById('hide-modal');
+      $hideModal.addEventListener('click', hideModal);
+
+      function hideModal() {
+        $overlay.classList.remove('active');
+        $modal.style.animation = 'modalOut .8s forwards';
+      }
+
+      function addEventClick($element) {
+        $element.addEventListener('click', function() {
+          showModal();
+        })
+      }
+
+      function renderMovieList (list, $container) {
+        $container.children[0].remove();
+        list.forEach((movie) => {
+          const HTMLString = videoItemTemplate(movie);
+          const movieElement = createTemplate(HTMLString);
+          $container.append(movieElement)
+          addEventClick(movieElement);
+        })
+      }
 
     const $actionContainer = document.querySelector('#action');
+    renderMovieList (actionList.data.movies, $actionContainer);
+
     const $dramaContainer = document.getElementById('drama');
+    renderMovieList (dramaList.data.movies, $dramaContainer);
+
     const $animationContainer = document.getElementById('animation');
-
-    const $featuringContainer = document.getElementById('featuring');
-    const $form = document.getElementById('form');
-    const $home = document.getElementById('home');
-
-
-    const $modal = document.getElementById('modal');
-    const $overlay = document.getElementById('overlay');
-    const $hideModal = document.getElementById('hide-modal');
+    renderMovieList (animationList.data.movies, $animationContainer);
 
     const $modalImage = $modal.querySelector('img');
     const $modalTitle = $modal.querySelector('h1');
